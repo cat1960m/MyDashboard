af44163c204305b97ae55da70b166280275a4f78
Kateryna Dobrovolska <kateryna.dobrovolska@Katerynas-MacBook-Pro.local>
2024-08-13 08:33:31 +0300

    chapter6

--- dashboard/starter-example/tailwind.config.ts
+++ dashboard/starter-example/tailwind.config.ts
@@ -17,6 +17,9 @@ const config: Config = {
           500: '#0070F3',
           600: '#2F6FEB',
         },
+        magenta: {
+          500: '#ff00ff',
+        },
       },
     },
     keyframes: {
